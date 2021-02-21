8b98956efc399e177a5a56ea11c9bbf638dd9082
kivimango <balazsviktor23@gmail.com>
2021-02-21 10:49:16 +0100

    Added Label to ui

--- examples/minimal.rs
+++ examples/minimal.rs
@@ -1,10 +1,15 @@
 use minifb::{Window, WindowOptions};
-use mygui::Application;
+use mygui::{Application, Label};
 
 fn main() {
     Application::new()
         .name("mygui minimal example")
         .window(Window::new("Minimal example", 500, 500, WindowOptions::default()).unwrap())
+        .ui(|ctx| {
+            let label = Label::new("sample text".to_string())
+            .build(ctx);
+            label.entity()
+        })
         .build()
         .run();
 }
