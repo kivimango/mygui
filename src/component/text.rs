88059513c918630b92b521d32e0db7e30a07fb00
kivimango <balazsviktor23@gmail.com>
2021-02-21 10:44:32 +0100

    Added TextComponent

---
+++ src/component/text.rs
@@ -0,0 +1,18 @@
+use specs::{Component, VecStorage};
+
+#[derive(Debug, Default, Clone)]
+pub struct TextComponent {
+    pub text: String
+}
+
+impl Component for TextComponent {
+    type Storage = VecStorage<Self>;
+}
+
+impl From<String> for TextComponent {
+    fn from(text: String) -> Self {
+        TextComponent {
+            text
+        }
+    }
+}
