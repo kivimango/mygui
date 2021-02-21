666a12cee533e25018fecea5e6b01108014d3234
kivimango <balazsviktor23@gmail.com>
2021-02-21 10:48:50 +0100

    Project structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
 mod application;
+mod build_context;
 mod component;
 mod system;
 mod widget;
+mod tree;
 
 pub use self::application::*;
+pub use self::build_context::*;
+pub use self::tree::*;
+pub use self::component::*;
+pub use self::system::*;
+pub use self::widget::*;
+pub use self::tree::*;
