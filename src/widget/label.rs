c5e0c4ee4bc471ea59cc60601d19593a72c4042b
kivimango <balazsviktor23@gmail.com>
2021-08-22 18:31:50 +0200

    Rework Label widget

--- src/widget/label.rs
+++ src/widget/label.rs
@@ -1,47 +1,14 @@
+use crate::{component::TextComponent, PositionComponent, RenderComponent, Shape, SizeComponent};
 use specs::{Builder, Entity, World, WorldExt};
-use crate::{PositionComponent, RenderComponent, Shape, SizeComponent, component::TextComponent};
 
 const LABEL_DEFAULT_WIDTH: usize = 150;
 const LABEL_DEFAULT_HEIGHT: usize = 55;
 
-pub struct Label {
-    entity: Entity,
-    position: PositionComponent,
-    render: RenderComponent,
-    size: SizeComponent,
-    text: TextComponent,
-}
+pub struct Label {}
 
 impl Label {
     pub fn new(text: String) -> LabelBuilder {
-        LabelBuilder::new()
-        .text(text)
-    }
-
-    pub fn entity(&self) -> Entity {
-        self.entity
-    }
-
-    pub fn position(&self) -> PositionComponent {
-        self.position
-    }
-
-    pub fn size(&self) -> SizeComponent {
-        self.size
-    }
-
-    pub fn text(&self) -> &TextComponent {
-        &self.text
-    }
-
-    pub fn text_mut(&mut self) -> &mut TextComponent {
-        &mut self.text
-    }
-
-    pub fn set_text(&mut self, new_text: String) {
-        self.text = TextComponent {
-            text: new_text
-        };
+        LabelBuilder::new().text(text)
     }
 }
 
@@ -57,58 +24,43 @@ impl LabelBuilder {
         LabelBuilder {
             position: PositionComponent::default(),
             render: RenderComponent {
-                shape: Shape::Rectangle
+                shape: Shape::Rectangle,
             },
             size: SizeComponent {
                 width: LABEL_DEFAULT_WIDTH,
-                height: LABEL_DEFAULT_HEIGHT
+                height: LABEL_DEFAULT_HEIGHT,
             },
             text: TextComponent::default(),
         }
     }
 
     pub fn position(mut self, x: usize, y: usize) -> LabelBuilder {
-        self.position = PositionComponent {
-            x,
-            y
-        };
+        self.position = PositionComponent { x, y };
         self
     }
 
     pub fn size(mut self, width: usize, height: usize) -> LabelBuilder {
-        self.size = SizeComponent {
-            width,
-            height
-        };
+        self.size = SizeComponent { width, height };
         self
     }
 
     pub fn text(mut self, text: String) -> Self {
-        self.text = TextComponent {
-            text
-        };
+        self.text = TextComponent { text };
         self
     }
 
-    pub fn build(self, world: &mut World) -> Label {
+    pub fn build(self, world: &mut World) -> Entity {
         world.register::<PositionComponent>();
         world.register::<RenderComponent>();
         world.register::<TextComponent>();
         world.register::<SizeComponent>();
 
-        let label = world.create_entity()
+        world
+            .create_entity()
             .with(self.position)
             .with(self.render)
             .with(self.size)
             .with(self.text.clone())
-            .build();
-
-        Label {
-            entity: label,
-            render: self.render,
-            position: self.position,
-            size: self.size,
-            text: self.text
-        }
+            .build()
     }
 }
