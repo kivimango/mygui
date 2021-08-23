297476e52100861b70bde64bad6f53618d605e27
kivimango <balazsviktor23@gmail.com>
2021-08-23 19:53:38 +0200

    Removed unused code

--- src/application.rs
+++ src/application.rs
@@ -12,31 +12,6 @@ impl Application {
 
     pub fn run(&mut self) {
         self.shell.run();
-
-        /*let (width, height) = (self.window.width(), self.window.height());
-        let buffer = Rc::new(RefCell::new(vec![0; width as usize * height as usize]));
-        let mut world = World::new();
-
-        if let Some(ui_builder) = &self.ui {
-            let root_widget = ui_builder(&mut world);
-            let root_node = Node::new(root_widget, None);
-            let _entity_tree = EntityTree::new(root_node);
-        }
-
-        let mut text_system = TextRenderSystem{};
-        let mut render_system = RenderingSystem::new(&buffer);*/
-
-        //self.window
-        //    .limit_update_rate(Some(std::time::Duration::from_micros(8300)));
-
-        /*while self.window.is_open() && !self.window.is_key_down(Key::Escape) {
-            render_system.run_now(&mut world);
-            text_system.run_now(&mut world);
-
-            self.window
-            .update_with_buffer(&buffer.borrow_mut(), 500, 500)
-            .unwrap();
-        }*/
     }
 }
 
