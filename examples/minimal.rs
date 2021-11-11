a30ece9209ccc5f2a87400b16c36547887c4e8ad
kivimango <balazsviktor23@gmail.com>
2021-11-11 11:47:59 +0100

    Try Button instead of Label

--- examples/minimal.rs
+++ examples/minimal.rs
@@ -1,4 +1,4 @@
-use mygui::{Application, Border, Label, Window};
+use mygui::{Application, Border, Button, Label, Window};
 use orbclient::{Color};
 
 fn main() {
@@ -9,7 +9,7 @@ fn main() {
                 .centered()
                 .title("Minimal example")
                 .ui(|world| {
-                    Label::new("sample text".to_string())
+                    /*Label::new("sample text".to_string())
                         .background(Color::rgb(125, 100, 75))
                         .border(Border::new()
                             .color( orbclient::Color::rgba(12, 255, 12, 255))
@@ -18,6 +18,12 @@ fn main() {
                         )
                         .position(5, 5)
                         .size(110, 75)
+                        .build(world)*/
+                        Button::new()
+                        .title("Button 1")
+                        .on_click(move |_, _| {
+                            println!("mouse click handler!");
+                        })
                         .build(world)
                 })
                 .build(),
