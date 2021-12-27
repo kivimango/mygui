5e8d3195694ed8607ca978e9f7b1f8559c21d9cc
kivimango <balazsviktor23@gmail.com>
2021-12-27 20:19:44 +0100

    Remove NoopLayout

--- src/widget/layout/mod.rs
+++ src/widget/layout/mod.rs
@@ -1,25 +1,3 @@
 mod center;
 
-use specs::{Entity, World};
-use crate::{Constraints, DesiredSize, Layout};
 pub use self::center::*;
-
-pub struct NoopLayout {}
-
-impl Layout for NoopLayout {
-    fn arrange(&self, _widget: Entity, _desired_size: &DesiredSize, _world: &World) {
-        
-    }
-
-    fn measure(&self, _entity: Entity, _constraints: &Constraints, _world: &World) -> DesiredSize {
-        
-        DesiredSize {dirty: false, width:0, height:0
-        }
-    }
-}
-
-impl NoopLayout {
-    pub fn new() -> Self {
-        NoopLayout {}
-    }
-}
