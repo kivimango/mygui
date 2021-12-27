93183967a16847a1e02019b7d971d60b4c3c7a6b
kivimango <balazsviktor23@gmail.com>
2021-12-27 19:52:30 +0100

    Update LayoutComponent during window resizing

--- src/shell.rs
+++ src/shell.rs
@@ -1,5 +1,5 @@
 use std::{cell::{Ref, RefCell}, rc::Rc};
-use crate::{Constraints, EntityTree, LayoutComponent, NoopLayout, RenderingSystem, Window, WindowComponent, layout_system};
+use crate::{Constraints, EntityTree, LayoutComponent, RenderingSystem, Window, WindowComponent, WindowLayout, layout_system};
 use orbclient::{Event, EventOption, Renderer, ResizeEvent};
 use specs::{Builder, Entity, RunNow, World, WorldExt};
 
@@ -43,21 +43,21 @@ impl Shell {
         let mut world = World::new();
         let mut tree = EntityTree::new();
         let root;
-        let root_id;
+        //let root_id;
         // TODO: workaround the Rc<RefCell>> mess
         {
             let wb = self.window.borrow();
             root = entity_for_window(wb, &mut world);
             tree.add_node(root);
-            root_id = tree.set_root(root);
+            tree.set_root(root);
             world.insert(tree);
         }
 
         if let Some(ui_builder) = self.window.borrow().ui() {
             let child_of_root = ui_builder(&mut world);
             let mut tree = world.write_resource::<EntityTree>();
-            let child_id = tree.add_node(child_of_root);
-            tree.append_child(root_id, child_id);
+            tree.add_node(child_of_root);
+            tree.append_child(root, child_of_root);
         }
 
         world.maintain();
@@ -87,11 +87,26 @@ impl Shell {
 }
 
 fn sync_window_size(window_id: Entity, world: &World, event: ResizeEvent) {
+    // TODO: add min and max size checking
     // updates the WindowComponent's size in the ECS world: PixMap needs it to be resize itself in the RenderingSystem
-    let mut storage = world.write_component::<WindowComponent>();
-    if let Some(window_component) = storage.get_mut(window_id) {
-        window_component.width = event.width;
-        window_component.height = event.height;
+    {
+        let mut layout_storage = world.write_component::<LayoutComponent>();
+        if let Some(window_constraints) = layout_storage.get_mut(window_id) {
+            window_constraints.constraints.max_width = event.width;
+            window_constraints.constraints.max_height = event.height;
+        } else {
+            eprintln!("Cannot update window constraints after window resizing!");
+        }
+    }
+
+    {
+        let mut window_storage = world.write_component::<WindowComponent>();
+        if let Some(window_component) = window_storage.get_mut(window_id) {
+            window_component.width = event.width;
+            window_component.height = event.height;
+        } else {
+            eprintln!("Cannot update window component's size after window resizing!");
+        }
     }
 }
 
@@ -106,7 +121,7 @@ fn entity_for_window(window: Ref<Window>, world: &mut World) -> Entity {
             max_width: window.max_width(),
             max_height: window.max_height(),
         },
-        object: Box::new(NoopLayout{})
+        object: Box::new(WindowLayout{})
     };
     let window_component = WindowComponent {
         id: window.inner().id(),
