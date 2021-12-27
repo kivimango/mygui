80f7823fd0fe020d59ec821c7b51a22e9e06b162
kivimango <balazsviktor23@gmail.com>
2021-12-27 20:21:26 +0100

    Rework EntityTree again

--- src/tree.rs
+++ src/tree.rs
@@ -1,10 +1,6 @@
-use std::{collections::HashMap, ops::Index};
+use std::{collections::HashMap};
 use specs::Entity;
-use indextree::{Arena, Children, Node, NodeError, NodeId};
-
-/// The index of the entity in the tree's arena.
-/// Wrapper over indextree::NodeId
-pub struct TreeIdx(pub NodeId);
+use indextree::{Arena, Children, Descendants, NodeError, NodeId};
 
 pub(crate) struct EntityTree {
     arena: Arena<Entity>,
@@ -23,58 +19,97 @@ impl EntityTree {
 
     /// Registers the node's entity in the tree and returns its id.
     /// The node does not have relationship with other nodes unless you call append_child().
-    pub fn add_node(&mut self, node: Entity) -> TreeIdx {
-        let idx = self.arena.new_node(node);
-        self.map.insert(node, idx);
-        TreeIdx {0: idx}
+    /// If the node's entity is already added to the tree, nothing happens.
+    /// For the alternate version of the method, use try_add_node().
+    pub fn add_node(&mut self, node: Entity) {
+        if !self.map.contains_key(&node) {
+            let idx = self.arena.new_node(node);
+            self.map.insert(node, idx);
+        }
     }
 
-    /// Appends `child` to `parent` in the tree.
+    /// Makes a parent-child relationship between two nodes in the tree.
+    /// Both nodes must be added to the tree before calling this method!
     ///
     /// # Panics
     /// This method is panics if the two node are the same, 
     /// or any of the nodes are removed.
     /// 
     /// For the non-panicking version, use try_append_child().
-    pub fn append_child(&mut self, parent: TreeIdx, child: TreeIdx) {
-        parent.0.append(child.0, &mut self.arena);
+    pub fn append_child(&mut self, parent: Entity, child: Entity) {
+        let parent_node = self.map.get(&parent)
+            .expect("Cannot append: The parent node not found! Add the node to the tree with add_node()!");
+        let child_node = self.map.get(&child)
+            .expect("Cannot append: The child node not found! Add the node to the tree with add_node()!");
+        parent_node.append(*child_node, &mut self.arena);
     }
 
     pub fn count(&self) -> usize {
         self.arena.count()
     }
 
-    pub fn child(&self, parent: TreeIdx) -> Children<Entity> {
-        parent.0.children(&self.arena)
+    pub fn child_of(&self, parent: Entity) -> Option<Entity> {
+        if let Some(node_id) = self.map.get(&parent) {
+            if let Some(child) = node_id.children(&self.arena).nth(0) {
+                if let Some(node) = self.arena.get(child) {
+                    return Some(*node.get())
+                } else {
+                    return None;
+                }
+            } else {
+                return None;
+            }
+        } else {
+            return None;
+        }
     }
 
-    pub fn children(&self) -> impl Iterator<Item = &Node<Entity>> {
-        self.arena.iter()
+    pub fn children_of(&self, parent: Entity) -> Children<Entity> {
+        let node = self.map.get(&parent);
+        node.unwrap().children(&self.arena)
     }
 
-    /* pub fn id(&self, node: Entity) -> NodeId {
-       
-    } */
+    pub fn children(&self) -> Descendants<Entity> {
+        let root = self.map.get(&self.root.unwrap()).unwrap();
+        root.descendants(&self.arena)
+    }
+
+    pub fn entitiy_of(&self, node: NodeId) -> Option<Entity> {
+        if let Some(node) = self.arena.get(node) {
+            return Some(*node.get());
+        }
+        None
+    }
 
     pub fn is_empty(&self) -> bool {
         self.arena.is_empty()
+    }
+
+    pub fn parent(&self, widget: Entity) -> Option<Entity> {
+        if let Some(node_idx) = self.map.get(&widget) {
+            if let Some(_i) = self.arena.get(*node_idx) {
+                if let Some(parent_idx) = node_idx.ancestors(&self.arena).skip(1).next() {
+                    if let Some(parent_entity) = self.arena.get(parent_idx) {
+                        return Some(*parent_entity.get())
+                    } else {
+                        return None;
+                    }
+                } else {
+                    return None;
+                }
+            } else {
+                return None;
+            }
+        } else {
+            return  None;
+        }
     }
 
     pub fn root(&self) -> Option<Entity> {
         self.root
     }
 
-    pub fn root_idx(&self) -> Option<TreeIdx> {
-        if let Some(root) = self.root() {
-            if let Some(id) = self.map.get(&root) {
-                return Some(TreeIdx {0: *id})
-            }
-            return None
-        }
-        None
-    }
-
-    pub fn set_root(&mut self, root: Entity) -> TreeIdx {
+    pub fn set_root(&mut self, root: Entity) {
         /* 
         if let Some(idx) = self.map.get(&root) {
             if let Some(node) = self.arena.get(*idx) {
@@ -91,7 +126,32 @@ impl EntityTree {
     pub fn try_append_child(&mut self, parent: NodeId, child: NodeId) -> Result<(), NodeError> {
         parent.checked_append(child, &mut self.arena)
     }
+
+    pub fn try_children_of(&self, parent: Entity) -> Option<Children<Entity>> {
+        if let Some(node) = self.map.get(&parent) {
+            return Some(node.children(&self.arena))
+        }
+        None
+    }
+
+    /* fn valid(&self, parent: &Entity, child: &Entity) -> bool {
+        self.map.contains_key(parent) && self.map.contains_key(child)
+    } */
 }
+
+/*pub struct TreeIter {
+    stack: Vec<Entity>
+}
+
+impl TreeIter {
+    fn new() -> TreeIter {
+
+    }
+}
+
+impl Iterator for TreeIter {
+    fn 
+}*/
 
 mod test {
     use crate::{EntityTree};
