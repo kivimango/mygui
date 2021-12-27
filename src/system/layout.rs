d48e82dd175018d38d7a8d093bf84700b6d75a8e
kivimango <balazsviktor23@gmail.com>
2021-12-27 19:55:08 +0100

    Rework: just call the root layout object instead of iterating the whole tree

--- src/system/layout.rs
+++ src/system/layout.rs
@@ -1,19 +1,14 @@
 use specs::{World, WorldExt};
-use crate::{Constraints, EntityTree, LayoutComponent, SizeComponent};
+use crate::{EntityTree, LayoutComponent};
 
-pub(crate) fn layout_system(world: &World) {
+pub fn layout_system(world: &World) {
     let layouts = world.read_storage::<LayoutComponent>();
-    let sizes = world.read_storage::<SizeComponent>();
     let tree = world.read_resource::<EntityTree>();
 
-    if let Some(_root) = tree.root() {
-        for node in tree.children() {
-            let entity = node.get();
-            if let Some(layout) = layouts.get(*entity) {
-                let desired_size = layout.object.measure(*entity, &layout.constraints, world);
-                println!("desired size for entity: {:?}:{:?}", desired_size, entity);
-                layout.object.arrange(*entity, &desired_size, world);
-            }
-        }
+    if let Some(root) = tree.root() {
+        let root_layout = layouts.get(root).expect("Root widget LayoutComponent not found !");
+        let window_constraints = root_layout.constraints;
+        let desired_child_size = root_layout.object.measure(root, &window_constraints, world);
+        root_layout.object.arrange(root, &desired_child_size, world);
     }
 }
